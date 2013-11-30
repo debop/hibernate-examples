919ab7edfe55f728290209ed5a616463bebfd654
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2013-11-30 13:44:43 +0900

    -. inheritance JoindSubclass 예제 추가.

---
+++ src/test/java/org/hibernate/examples/mapping/inheritance/subclass/SubclassTest.java
@@ -0,0 +1,10 @@
+package org.hibernate.examples.mapping.inheritance.subclass;
+
+/**
+ * org.hibernate.examples.mapping.inheritance.subclass.SubclassTest
+ *
+ * @author 배성혁 sunghyouk.bae@gmail.com
+ * @since 2013. 11. 30. 오후 12:53
+ */
+public class SubclassTest {
+}
---
+++ src/test/java/org/hibernate/examples/mapping/inheritance/unionsubclass/UnionSubclassTest.java
@@ -0,0 +1,10 @@
+package org.hibernate.examples.mapping.inheritance.unionsubclass;
+
+/**
+ * org.hibernate.examples.mapping.inheritance.unionsubclass.UnionSubclassTest
+ *
+ * @author 배성혁 sunghyouk.bae@gmail.com
+ * @since 2013. 11. 30. 오후 12:53
+ */
+public class UnionSubclassTest {
+}
